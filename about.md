b869218ff87d2aba44e421da4af3ad22d621b4fc
Emery <emery.jacobowitz@gmail.com>
2025-04-30 19:41:39 +0000

    initial commit

---
+++ about.md
@@ -0,0 +1,24 @@
+---
+title: About Me
+---
+
+## Bio
+
+I'm a graduate student studying computational linguistics. I'm interested in NLP for low-resource languages, and the curation of high-quality datasets.
+
+## Projects
+This section will be filled in as I collect my code :\)
+
+- ViPER: A simple functional programming language with unit testing. Compiler hosted in `OCaml` targeting `x86_64` assembly with a `C` runtime. 
+- NLP Games: A collection of toys illustrating various NLP tools and concepts. Includes Twenty Questions and a Connections generator.
+
+## Hobbies
+
+- I enjoy word games, in particular constructing and solving crossword puzzles.
+- I love learning languages. Some of my favorites are Finnish, Hebrew, Scottish Gaelic, and Sumerian.
+- In spite of my general clumsiness, I've learned a handful of yo-yo tricks.
+- I'm in the high 99th percentile of users on [JetPunk](https://www.jetpunk.com)
+
+## Links
+
+- [GitHub](https://github.com/ThePineappleW)
---
+++ site.hs
@@ -0,0 +1,90 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Monoid (mappend)
+import Hakyll
+import Hakyll.Favicon (faviconsField, faviconsRules)
+import Text.Pandoc.Highlighting (Style, haddock, styleToCss)
+import Text.Pandoc.Options (ReaderOptions (..), WriterOptions (..))
+
+--------------------------------------------------------------------------------
+pandocCodeStyle :: Style
+pandocCodeStyle = haddock
+
+pandocCompiler' :: Compiler (Item String)
+pandocCompiler' =
+  pandocCompilerWith
+    defaultHakyllReaderOptions
+    defaultHakyllWriterOptions
+      { writerHighlightStyle = Just pandocCodeStyle
+      }
+
+defaultContext' :: Context String
+defaultContext' = faviconsField `mappend` defaultContext
+
+main :: IO ()
+main = hakyll $ do
+  faviconsRules "images/epsilon.svg"
+
+  create ["css/syntax.css"] $ do
+    route idRoute
+    compile $ do
+      makeItem $ styleToCss pandocCodeStyle
+
+  match "images/*" $ do
+    route idRoute
+    compile copyFileCompiler
+
+  match "css/*" $ do
+    route idRoute
+    compile compressCssCompiler
+
+  match (fromList ["about.md"]) $ do
+    route $ setExtension "html"
+    compile $
+      pandocCompiler'
+        >>= loadAndApplyTemplate "templates/default.html" defaultContext'
+        >>= relativizeUrls
+
+  match "posts/**.md" $ do
+    route $ setExtension "html"
+    compile $
+      pandocCompiler'
+        >>= loadAndApplyTemplate "templates/post.html" postCtx
+        >>= loadAndApplyTemplate "templates/default.html" postCtx
+        >>= relativizeUrls
+
+  create ["blog.html"] $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let archiveCtx =
+            listField "posts" postCtx (return posts)
+              `mappend` constField "title" "Archives"
+              `mappend` defaultContext'
+
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/blog.html" archiveCtx
+        >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+        >>= relativizeUrls
+
+  match "index.html" $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let indexCtx =
+            listField "posts" postCtx (return posts)
+              `mappend` defaultContext'
+
+      getResourceBody
+        >>= applyAsTemplate indexCtx
+        >>= loadAndApplyTemplate "templates/default.html" indexCtx
+        >>= relativizeUrls
+
+  match "templates/*" $ compile templateBodyCompiler
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+  dateField "date" "%B %e, %Y"
+    `mappend` defaultContext'
