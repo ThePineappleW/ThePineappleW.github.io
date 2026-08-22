6f2844dc8540c129b0e715631c935cecd91da05c
Emery <57153066+ThePineappleW@users.noreply.github.com>
2026-08-22 02:23:11 +0000

    Apply ormolu formatting.

--- Crossword.hs
+++ Crossword.hs
@@ -2,97 +2,103 @@
 
 module Crossword where
 
-import Data.List as L
+import Data.Aeson (Value, decode, encode, withObject)
+import qualified Data.Base64.Types as B64
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
-import qualified Data.Text.Encoding.Base64 as B64
+import qualified Data.ByteString.Lazy.Char8 as L8
+import Data.Char (isAlpha)
+import Data.List as L
 import qualified Data.Text as T
+import qualified Data.Text.Encoding.Base64 as B64
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.Encoding as TLE
-import qualified Data.ByteString.Lazy.Char8 as L8
-import qualified Data.Base64.Types as B64
-import Data.Yaml (decodeEither', FromJSON(..), (.:), (.:?), (.!=), ParseException)
-import Hakyll (Context, Compiler, Item (itemIdentifier, itemBody), field, toFilePath, getResourceLBS, constField, makeItem, loadAndApplyTemplate, relativizeUrls)
-import Data.Aeson (Value, encode, decode, withObject)
-import Data.Char (isAlpha)
+import Data.Yaml (FromJSON (..), ParseException, decodeEither', (.!=), (.:), (.:?))
+import Hakyll (Compiler, Context, Item (itemBody, itemIdentifier), constField, field, getResourceLBS, loadAndApplyTemplate, makeItem, relativizeUrls, toFilePath)
 import System.FilePath (takeBaseName)
 
 --------------------------------------------------------------------------------
 -- Crossword puzzle functions
 --------------------------------------------------------------------------------
 
-data Puzzle =
-    Puzzle  { title   :: T.Text
-            , date    :: T.Text
-            , kind    :: T.Text
-            , comment :: Maybe T.Text
-            , board   :: T.Text
-            , across  :: [T.Text]
-            , down    :: [T.Text]
-    }
+data Puzzle
+  = Puzzle
+  { title :: T.Text,
+    date :: T.Text,
+    kind :: T.Text,
+    comment :: Maybe T.Text,
+    board :: T.Text,
+    across :: [T.Text],
+    down :: [T.Text]
+  }
 
 instance FromJSON Puzzle where
   parseJSON = withObject "Puzzle" $ \o ->
-    Puzzle <$> o .: "title"
-           <*> o .: "date"
-           <*> o .:? "kind" .!= "standard"
-           <*> o .:? "comment"
-           <*> o .: "board"
-           <*> o .: "across"
-           <*> o .: "down"
+    Puzzle
+      <$> o .: "title"
+      <*> o .: "date"
+      <*> o .:? "kind" .!= "standard"
+      <*> o .:? "comment"
+      <*> o .: "board"
+      <*> o .: "across"
+      <*> o .: "down"
 
 puzzleDimensions :: T.Text -> (Int, Int)
 puzzleDimensions str =
-  let lines = T.lines str in
-  case lines of
-    [] -> (0, 0)
-    first:_ -> (length lines, T.length first)
+  let lines = T.lines str
+   in case lines of
+        [] -> (0, 0)
+        first : _ -> (length lines, T.length first)
 
 wallPositions :: T.Text -> [[Int]]
 wallPositions str =
-  let lines = T.lines str in
-    foldl (\acc (i, row) -> acc ++ map (\col_id -> [i, col_id]) (L.elemIndices '#' (T.unpack row)))
-     []
-     (zip [0..] lines)
+  let lines = T.lines str
+   in foldl
+        (\acc (i, row) -> acc ++ map (\col_id -> [i, col_id]) (L.elemIndices '#' (T.unpack row)))
+        []
+        (zip [0 ..] lines)
 
 b64Hash :: T.Text -> String
 b64Hash txt =
-  let onlyAlpha = T.toUpper $ T.filter isAlpha txt in
-    T.unpack $ B64.extractBase64 (B64.encodeBase64 onlyAlpha)
+  let onlyAlpha = T.toUpper $ T.filter isAlpha txt
+   in T.unpack $ B64.extractBase64 (B64.encodeBase64 onlyAlpha)
 
 puzzleCtx :: Context String -> Context String
 puzzleCtx baseCtx =
-  let stripDate = L.drop 11 in
-  let enspace = map (\c -> if c == '_' then ' ' else c) in
-  field "title" (\item -> do
-    let path = toFilePath (itemIdentifier item)
-    let title = enspace $ stripDate $ takeBaseName path
-    return title)
-  `mappend` baseCtx
+  let stripDate = L.drop 11
+   in let enspace = map (\c -> if c == '_' then ' ' else c)
+       in field
+            "title"
+            ( \item -> do
+                let path = toFilePath (itemIdentifier item)
+                let title = enspace $ stripDate $ takeBaseName path
+                return title
+            )
+            `mappend` baseCtx
 
 puzzleCompiler :: Context String -> Compiler (Item String)
 puzzleCompiler defaultContext = do
-    item <- getResourceLBS
-    let yamlBytes = BS.toStrict $ itemBody item
+  item <- getResourceLBS
+  let yamlBytes = BS.toStrict $ itemBody item
 
-    case decodeEither' yamlBytes of
-        Left err -> fail $ "YAML parse error: " ++ show err
-        Right puzzle -> do
-            let (nrows, ncols) = puzzleDimensions (board puzzle)
-            let walls = wallPositions (board puzzle)
-            let puzzleHash = b64Hash (board puzzle)
-            let cluesCtx =
-                    constField "title" (T.unpack (title puzzle)) <>
-                    constField "date" (T.unpack (date puzzle)) <>
-                    constField "rows" (show nrows) <>
-                    constField "columns" (show ncols) <>
-                    constField "hash" (show puzzleHash) <>
-                    constField "walls" (show walls) <>
-                    constField "across" (TL.unpack (TLE.decodeUtf8 (encode (across puzzle)))) <>
-                    constField "down" (TL.unpack (TLE.decodeUtf8 (encode (down puzzle)))) <>
-                    defaultContext
+  case decodeEither' yamlBytes of
+    Left err -> fail $ "YAML parse error: " ++ show err
+    Right puzzle -> do
+      let (nrows, ncols) = puzzleDimensions (board puzzle)
+      let walls = wallPositions (board puzzle)
+      let puzzleHash = b64Hash (board puzzle)
+      let cluesCtx =
+            constField "title" (T.unpack (title puzzle))
+              <> constField "date" (T.unpack (date puzzle))
+              <> constField "rows" (show nrows)
+              <> constField "columns" (show ncols)
+              <> constField "hash" (show puzzleHash)
+              <> constField "walls" (show walls)
+              <> constField "across" (TL.unpack (TLE.decodeUtf8 (encode (across puzzle))))
+              <> constField "down" (TL.unpack (TLE.decodeUtf8 (encode (down puzzle))))
+              <> defaultContext
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/puzzles/crossword.html" cluesCtx
-                >>= loadAndApplyTemplate "templates/default.html" cluesCtx
-                >>= relativizeUrls
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/puzzles/crossword.html" cluesCtx
+        >>= loadAndApplyTemplate "templates/default.html" cluesCtx
+        >>= relativizeUrls
--- site.hs
+++ site.hs
@@ -2,35 +2,34 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TupleSections #-}
 
-import GHC.Generics
-import qualified Data.ByteString.Lazy.Char8 as C
-import Data.Aeson (Value, encode, decode, withObject)
-import Data.Yaml (decodeEither', FromJSON(..), (.:), (.:?), (.!=), ParseException)
-import Data.List as L
+import Control.Exception (SomeException, evaluate, try)
+import Crossword (puzzleCompiler, puzzleCtx)
+import Data.Aeson (Value, decode, encode, withObject)
+import qualified Data.Base64.Types as B64
+import Data.Bits (Bits (..))
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
-import qualified Data.Text.Encoding.Base64 as B64
+import qualified Data.ByteString.Lazy.Char8 as C
+import qualified Data.ByteString.Lazy.Char8 as L8
+import Data.Char (isAlpha, ord)
+import Data.Int (Int32)
+import Data.List as L
+import Data.Maybe (fromMaybe)
+import Data.Monoid (mappend)
 import qualified Data.Text as T
+import qualified Data.Text.Encoding.Base64 as B64
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.Encoding as TLE
-import qualified Data.ByteString.Lazy.Char8 as L8
-import Data.Monoid (mappend)
+import Data.Yaml (FromJSON (..), ParseException, decodeEither', (.!=), (.:), (.:?))
+import GHC.Generics
+import GHC.IO.Encoding (setLocaleEncoding, utf8)
 import Hakyll
 import Hakyll.Favicon (faviconsField, faviconsRules)
+import System.FilePath (takeBaseName)
+import System.IO.Unsafe (unsafePerformIO)
 import Text.Jasmine
 import Text.Pandoc.Highlighting (Style, haddock, styleToCss)
 import Text.Pandoc.Options (Extension (..), HTMLMathMethod (..), ReaderOptions (..), WriterOptions (..), disableExtension, enableExtension)
-import Control.Exception (SomeException, try, evaluate)
-import System.IO.Unsafe (unsafePerformIO)
-import Data.Int (Int32)
-import Data.Bits (Bits(..))
-import Data.Char (ord, isAlpha)
-import qualified Data.Base64.Types as B64
-import GHC.IO.Encoding (setLocaleEncoding, utf8)
-import System.FilePath (takeBaseName)
-import Data.Maybe (fromMaybe)
-
-import Crossword (puzzleCtx, puzzleCompiler)
 
 --------------------------------------------------------------------------------
 -- Extension management
@@ -79,7 +78,6 @@ pandocCompiler' =
         writerExtensions = newExtensions
       }
 
-
 defaultContext' :: Context String
 defaultContext' = faviconsField `mappend` defaultContext
 
@@ -122,25 +120,24 @@ puzzleCompiler' = puzzleCompiler defaultContext'
 
 sortByRank :: [Item a] -> Compiler [Item a]
 sortByRank items = do
-    withRank <- mapM addRank items
-    return $ map snd $ sortOn fst withRank
+  withRank <- mapM addRank items
+  return $ map snd $ sortOn fst withRank
   where
     addRank item = do
-        rankStr <- getMetadataField (itemIdentifier item) "rank"
-        let n = fromMaybe maxBound (rankStr >>= readMaybe)
-        return (n :: Int, item)
+      rankStr <- getMetadataField (itemIdentifier item) "rank"
+      let n = fromMaybe maxBound (rankStr >>= readMaybe)
+      return (n :: Int, item)
 
     readMaybe s = case reads s of
-        [(x, "")] -> Just x
-        _         -> Nothing
+      [(x, "")] -> Just x
+      _ -> Nothing
 
 projectCompiler :: Compiler (Item String)
 projectCompiler = do
-    pandocCompiler'
-          >>= loadAndApplyTemplate "templates/projects/project.html" postCtx
-          >>= loadAndApplyTemplate "templates/default.html" postCtx
-          >>= relativizeUrls
-
+  pandocCompiler'
+    >>= loadAndApplyTemplate "templates/projects/project.html" postCtx
+    >>= loadAndApplyTemplate "templates/default.html" postCtx
+    >>= relativizeUrls
 
 --------------------------------------------------------------------------------
 
@@ -177,7 +174,7 @@ main = do
     match "projects/**.md" $ do
       route $ setExtension "html"
       compile projectCompiler
-    
+
     match "projects/**.pdf" $ do
       route idRoute
       compile copyFileCompiler
@@ -229,7 +226,7 @@ main = do
         -- However, Hakyll can't automatically recognize a YAML date field unless it's in the file name.
         -- But we don't want the date in the label when we have it on the site.
         -- So we have to do a little bit of meshugas to get rid of that part.
-        puzzles <- recentFirst =<< loadAll "puzzles/**.yaml"        
+        puzzles <- recentFirst =<< loadAll "puzzles/**.yaml"
         let puzzleArchiveCtx =
               listField "puzzles" puzzleCtx' (return puzzles)
                 `mappend` constField "title" "Puzzle Archives"
@@ -243,19 +240,19 @@ main = do
       route idRoute
       compile $ do
         -- Only take the first 5 of each type on the main page.
-        posts    <- fmap (take 5) (recentFirst =<< loadAll "posts/*.md")
+        posts <- fmap (take 5) (recentFirst =<< loadAll "posts/*.md")
         projects <- fmap (take 5) (sortByRank =<< loadAll "projects/**.md")
-        puzzles  <- fmap (take 5) (recentFirst =<< loadAll "puzzles/**.yaml")
+        puzzles <- fmap (take 5) (recentFirst =<< loadAll "puzzles/**.yaml")
         let indexCtx =
-              listField "posts" postCtx (return posts) <>
-              listField "projects" defaultContext' (return projects) <>
-              listField "puzzles" puzzleCtx' (return puzzles) <>
-              defaultContext'
+              listField "posts" postCtx (return posts)
+                <> listField "projects" defaultContext' (return projects)
+                <> listField "puzzles" puzzleCtx' (return puzzles)
+                <> defaultContext'
 
         getResourceBody
-            >>= applyAsTemplate indexCtx
-            >>= loadAndApplyTemplate "templates/default.html" indexCtx
-            >>= relativizeUrls
+          >>= applyAsTemplate indexCtx
+          >>= loadAndApplyTemplate "templates/default.html" indexCtx
+          >>= relativizeUrls
 
     match "templates/**" $ compile templateBodyCompiler
 
